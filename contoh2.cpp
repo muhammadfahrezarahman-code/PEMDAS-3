8408ff2add08d3c8195a0b3f5bef07b942779a6e
muhammadfahrezarahman-code <anwarfahreza4@gmail.com>
2026-03-02 12:51:04 +0700

    Memasukkan void output

--- contoh2.cpp
+++ contoh2.cpp
@@ -9,3 +9,13 @@ void input()
     cout << "Masukkan Jari-Jari :";
     cin >> jarijari;
 }
+
+float luasLingkaran(int a, float b)
+{
+    return a * a * b;
+}
+
+void output()
+{
+    cout << "Hasilnya : " << luasLingkaran(jarijari, phi);
+}
